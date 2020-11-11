1bb43a4eb22570074981dcc72f1bf414c307dc6d
angelcr25 <angelsamuelcr25@gmail.com>
2020-11-11 14:54:20 -0300

    1er commit

---
+++ ClassLibrary1/Bolillero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreBolillero
+{
+    public class Bolillero
+    {
+        public List<byte> adentro { get; set; }
+        public List<byte> afuera { get; set; }
+
+        private Random r { get; set; }
+
+        
+        public Bolillero()
+        {
+            var r = new Random();
+
+        }
+
+        private void cargarBolillero(byte inicio, byte fin)
+        {
+            for (byte i = inicio; i < fin; i++)
+            {
+                adentro.Add(i);
+            }
+        }
+
+        public byte SacarBolilla()
+        {
+            var indice = 0;
+            indice = r.Next(0, adentro.Count);
+            var bolilla = adentro[indice];
+            return bolilla;
+        }
+        public void Reingresar()
+        {
+            adentro.AddRange(afuera);
+            afuera.Clear();
+        }
+    }
+}
---
+++ ClassLibrary1/Simulacion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBolillero
+{
+    class Simulacion
+    {
+        Bolillero bolillero = new Bolillero();
+
+        
+        public bool jugar (List<byte> jugadas)
+        {
+            var comparar = 0;
+            bolillero.Reingresar();
+            foreach (var bolillaJugada in jugadas)
+            {
+                comparar = bolillero.SacarBolilla();
+
+                if (comparar != bolillaJugada)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public long jugarNveces(List<byte> jugadas, long cantJugar)
+        {
+            long cantGanados = 0;
+
+            for (long i = 0; i < cantJugar; i++)
+            {
+                if (this.jugar(jugadas) ==  true)
+                {
+                    cantGanados++;
+                }
+            }
+
+            return cantGanados;
+        }
+    }
+}
